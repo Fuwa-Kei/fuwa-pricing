080642ca913721685b6544e63351445295c51515
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-03-18 20:51:55 +0000

    Pricing page header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => {
+
+
+  return (
+    <div className="header">
+        <div className="headerBlock">
+            <h1>Online courses</h1>
+            <p>Get all lesson videos together with theory classes and recipes with each subscription. Cancel anytime   </p>
+        </div>
+        <div className="cta">
+            <div className="btn month">Monthly</div>
+            <div className="btn year">Annually*</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
